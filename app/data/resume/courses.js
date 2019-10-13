84d5ef01825ee5eca9cd6c50580b49cd22c79789
josipkoncurat <jkoncura@gmail.com>
2019-10-13 20:44:18 +0200

    Resume page is done

--- app/data/resume/courses.js
+++ app/data/resume/courses.js
@@ -1,120 +1,35 @@
 const courses = [
   {
-    title: 'Convex Optimization',
-    number: 'EE 364a',
-    link: 'http://stanford.edu/class/ee364a/',
-    univerity: 'Stanford',
+    title: 'Introduction to Computer Science and Programming Using Python',
+    number: 'MIT',
+    link: 'https://www.edx.org/course/6-00-1x-introduction-to-computer-science-and-programming-using-python-3',
+    university: 'MIT',
   }, {
-    title: 'Machine Learning',
-    number: 'CS 229',
-    link: 'http://cs229.stanford.edu/',
-    univerity: 'Stanford',
+    title: 'Object-Oriented programming with Java, part I',
+    number: 'University of Helsinki',
+    link: 'https://moocfi.github.io/courses/2013/programming-part-1/',
+    university: 'University of Helsinki',
   }, {
-    title: 'Convolutional Neural Networks for Visual Recognition',
-    number: 'CS 231n',
-    link: 'http://cs231n.stanford.edu/',
-    univerity: 'Stanford',
+    title: 'Object-Oriented programming with Java, part II',
+    number: 'University of Helsinki',
+    link: 'http://moocfi.github.io/courses/2013/programming-part-2/',
+    university: 'University of Helsinki',
   }, {
-    title: 'Numerical Linear Algebra',
-    number: 'CME 302',
-    link: 'http://scpd.stanford.edu/search/publicCourseSearchDetails.do;jsessionid=561188A06434D7D97953C4706DE12831?method=load&courseId=11685',
-    univerity: 'Stanford',
+    title: 'Version Control with Git',
+    number: 'Udacity',
+    link: 'https://www.udacity.com/course/version-control-with-git--ud123',
+    univerity: 'Udacity',
   }, {
-    title: 'Numerical Optimization',
-    number: 'CME 304',
-    link: 'http://web.stanford.edu/class/cme304/',
-    univerity: 'Stanford',
+    title: 'Android Basics by Google',
+    number: 'Udacity',
+    link: 'https://www.udacity.com/course/android-basics-nanodegree-by-google--nd803',
+    univerity: 'Udacity',
   }, {
-    title: 'Discrete Mathematics and Algorithms',
-    number: 'CME 305',
-    link: 'http://stanford.edu/~rezab/discrete/',
-    univerity: 'Stanford',
-  }, {
-    title: 'PDE’s of Applied Mathematics',
-    number: 'CME 303',
-    link: 'http://web.stanford.edu/class/math220/index.html',
-    univerity: 'Stanford',
-  }, {
-    title: 'Introduction to Linear Dynamical Systems',
-    number: 'EE 263',
-    link: 'http://ee263.stanford.edu/',
-    univerity: 'Stanford',
-  }, {
-    title: 'Introduction to Statistical Signal Processing',
-    number: 'EE 278B',
-    link: 'http://web.stanford.edu/class/ee278/',
-    univerity: 'Stanford',
-  }, {
-    title: 'Spacecraft Design',
-    number: 'AA 236A',
-    link: '',
-    univerity: 'Stanford',
-  }, {
-    title: 'Advanced Programming',
-    number: 'CME 212',
-    link: '',
-    univerity: 'Stanford',
-  }, {
-    title: 'Design Theory and Methodology',
-    number: 'MAE 397',
-    link: 'https://web.stanford.edu/group/designx_lab/cgi-bin/mainwiki/index.php/ME397_Design_Theory_%26_Methodology_Seminar',
-    univerity: 'Stanford',
-  }, {
-    title: 'Software Engineering Concepts',
-    number: 'CSE 442',
-    link: '',
-    univerity: 'Buffalo',
-  }, {
-    title: 'Hardware/Software Integrated System Design',
-    number: 'CSE 453',
-    link: '',
-    univerity: 'Buffalo',
-  }, {
-    title: 'Data Structures',
-    number: 'CS 250',
-    link: 'http://www.cse.buffalo.edu/~hungngo/classes/2013/Fall/250/',
-    univerity: 'Buffalo',
-  }, {
-    title: 'Introduction to Digital Signal Processing',
-    number: 'EE 516',
-    link: '',
-    univerity: 'Buffalo',
-  }, {
-    title: 'Computer Vision and Image Processing',
-    number: 'CSE 573',
-    link: 'http://cubs.buffalo.edu/~inwogu/teaching/Coursepage573_fa14/',
-    univerity: 'Buffalo',
-  }, {
-    title: 'Realtime Embedded Systems',
-    number: 'CSE 321',
-    link: '',
-    univerity: 'Buffalo',
-  }, {
-    title: 'Computer Architecture',
-    number: 'CSE 590',
-    link: 'http://www.cse.buffalo.edu/~stevko/courses/cse490/spring11/',
-    univerity: 'Buffalo',
-  }, {
-    title: 'Small Data',
-    number: 'MS&E 226',
-    link: 'http://web.stanford.edu/class/msande226/',
-    univerity: 'Stanford',
-  }, {
-    title: 'Stochastic Control',
-    number: 'EE 266',
-    link: 'http://ee266.stanford.edu/',
-    univerity: 'Stanford',
-  }, {
-    title: 'Simulation',
-    number: 'MS&E 223',
-    link: 'http://web.stanford.edu/class/msande223/handout.htm',
-    univerity: 'Stanford',
-  }, {
-    title: 'Deep Learning for Natural Language Processing',
-    number: 'CS 224d',
-    link: 'http://cs224d.stanford.edu/',
-    univerity: 'Stanford',
-  },
+    title: 'Java SE 8 Progammer',
+    number: 'Oracle',
+    link: 'https://education.oracle.com/java-se-8-programmer-i/pexam_1Z0-808',
+    univerity: 'Oracle',
+  }
 ];
 
 export default courses;
--- app/data/resume/degrees.js
+++ app/data/resume/degrees.js
@@ -1,14 +1,14 @@
 const degrees = [
   {
-    school: 'Stanford University',
-    degree: 'M.S. Computational and Mathematical Engineering',
-    link: 'https://stanford.edu',
-    year: 2016,
+    school: 'Faculty of Mining, Geology and Petroleum Engineering, Zagreb',
+    degree: 'M.S. Petroleum Engineering',
+    link: 'https://www.rgn.unizg.hr/en/',
+    year: 2015,
   }, {
-    school: 'University at Buffalo',
-    degree: 'B.S. Electrical Engineering, Computer Engineering',
-    link: 'https://buffalo.edu',
-    year: 2012,
+    school: 'Faculty of Mining, Geology and Petroleum Engineering, Zagreb',
+    degree: 'B.S. Petroleum Engineering',
+    link: 'https://www.rgn.unizg.hr/en/',
+    year: 2013,
   },
 ];
 

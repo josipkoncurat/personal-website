81961760486b5289ba2b7094d85e41dcc20d90e9
josipkoncurat <jkoncura@gmail.com>
2019-11-03 13:30:12 +0100

    commented out image on projects since it doesn't render, to be fixed

--- app/components/Projects/Cell.js
+++ app/components/Projects/Cell.js
@@ -10,9 +10,9 @@ const Cell = ({ data }) => (
         <h3><a href={data.link}>{data.title}</a></h3>
         <time className="published">{dayjs(data.date).format('MMMM, YYYY')}</time>
       </header>
-      <a href={data.link} className="image">
+      {/* <a href={data.link} className="image">
         <img src={url.resolve(BASE_PATH, data.image)} alt={data.title} />
-      </a>
+      </a> */}
       {/* <div className="description">
         <p>{data.desc}</p>
       </div> */}
